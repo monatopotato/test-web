71607307a746dd7b5db3df4c57aedadd0c8d24d9
monatopotato <65310139+monatopotato@users.noreply.github.com>
2023-08-29 23:09:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const track = document.getElementById("image-track")
 
 window.onmousedown = e =>{
-  track.dataset.mouseDownAt = e.clientX;
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
+        maxDelta = window.innerWidth / 2;
+  const percentage = (mouseDelta / maxDelta) * 100;
   
 }
