8511243971a016a6cabf0c22a210ad8c92b7a585
monatopotato <65310139+monatopotato@users.noreply.github.com>
2023-08-29 23:19:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,23 @@ window.onmousedown = e => {
   track.dataset.mouseDownAt = e.clientX;
 }
 
+window.onmouseup = () => {
+  track.dataset.mouseDownAt = "0";
+  track.dataset.prevPercentage = track.dataset.precentage;
+}
+
 window.onmousemove = e =>{
   if(track.dataset.mouseDownAt === "0") return;
   
   const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
         maxDelta = window.innerWidth / 2;
+  
   const percentage = (mouseDelta / maxDelta) * -100;
-
+        nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+        Math.min(nextPercentage,0);
+        Math.max(nextPercentage, -100);
+        
+  track.dataset.precentage = nextPercentage;
+  
   track.style.transform = 'translate(${percentage}%, -50%)';
-  
-  
 }
